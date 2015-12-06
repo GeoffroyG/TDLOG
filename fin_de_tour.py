c48671b5b9a4f99af5725c03111bff4a49f9e39c
GeoffroyG <geoffroy.gallier@wanadoo.fr>
2015-12-06 11:41:00 +0100

    Fonction de fin de tour basique - v2
    Ajout d'une fonction qui détecte si le joueur a gagné ou pas

--- fin_de_tour.py
+++ fin_de_tour.py
@@ -8,6 +8,11 @@ Variables globales à initialiser :
 - nb_tours le compteur du nombre de tours
 - hab_par_maison la capacité d'accueil de chaque maison
 - stock_bois le bois stocké
+- condition_victoire le nombre de maisons nécessaires pour gagner le jeu
+
+Fonctions à définir : 
+- fermer_jeu() la fonction qui affiche un message "gagné" et ferme proprement le jeu
+
 """
 
 
@@ -32,3 +37,20 @@ def fin_de_tour():
             stock_bois += int(batiments[i].prod_max * batiments[i].hab_affectes / batiments[i].hab_requis)
             
     nb_tours += 1
+    
+    if fin_de_jeu():
+        fermer_jeu()
+
+def fin_de_jeu():
+    nb_maisons = 0
+    for i in range(taille_plateau):
+        if batiments[i] == 1: # 1 représente la maison
+            nb_maisons += 1
+            
+    gagne = False
+
+    if nb_maisons >= condition_victoire:
+        gagne = True
+    return gagne
+    
+    
