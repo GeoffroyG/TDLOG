487936dfef25712d586124d7d64349f6e20449a8
GeoffroyG <geoffroy.gallier@wanadoo.fr>
2015-12-10 18:06:18 +0100

    Classes et Tests
    Non fonctionnel, en cours de correction

--- TDLOG-Tests-et-classes/Classes et Tests.py
+++ TDLOG-Tests-et-classes/Classes et Tests.py
@@ -4,12 +4,19 @@
 # 2 designe une  usine sur l'interface "batiments"
 # 3 designe un batiment en général sur l'interface "batiments"
 
+batiments=[[9,9,9,9,9,9,9,9],
+           [9,9,9,9,9,9,9,9],
+           [0,0,0,9,9,9,9,9],
+           [9,9,9,9,9,9,9,9]]
+
+
 taille_hauteur=4  # pour le besoin de l'exercice
 taille_largeur=8  # pour le besoin de l'exercice
 
 stock_bois=0 # evolution avec le temps
 hab_restant=0 # evolution avec le temps
 
+maison1 = Maison(3,4)
 
 class Batiment():
     def __init__(self, i, j):
@@ -40,21 +47,16 @@ class Batiment():
 
     def accept_construction(self, i, j):
         result= False
-        if (self.check_emplacement_vide(i, j)) and (self.check_raccord_route(i, j)):
+        if (self.check_emplacement_vide(i, j)) And (self.check_raccord_route(i, j)):
             result= True
         return(result) 
 
 #    def affiche_changement(self)
 
-class Vide(Batiment):
-    def __init__(self):
-        self.type = 9
-        
-    
           
 class Maison(Batiment):
-    def __init__(self, i, j):
-        super().__init__(i, j)
+    def __init__(self):
+        super().__init__()
         self.type = 1
         self.hab_par_maison=5
         self.materiaux_requis=3
@@ -66,15 +68,15 @@ class Maison(Batiment):
         return(result)
 
     def creation(self):
-        if self.accept_construction() and self.check_ressource():
+        if self.accept_construction() And self.check_ressource():
             batiments[self.i][self.j].type = 1
 
     def destruction(self):
         batiments[self.i][self.j].type = 9  # destruction variable selon batiment_autres ressources specifiques (incomplet)
 
 class Usine(Batiment):
-    def __init__(self, i, j):
-        super().__init__(i, j)
+    def __init__(self):
+        super().__init__()
         self.type = 2
         self.hab_requis = 10
         self.hab_affectes = 0
@@ -84,20 +86,20 @@ class Usine(Batiment):
 
     def check_ressource(self):
         result= False
-        if (stock_bois >= self.materiaux_requis) and (hab_restant >= 1):
+        if (stock_bois >= self.materiaux_requis) And (hab_restant >= 1):
             result= True
         return(result)
 
     def creation(self):
-        if self.accept_construction() and self.check_ressource():
+        if self.accept_construction() And self.check_ressource():
             batiments[self.i][self.j].type = 2
  
     def destruction(self):
         batiments[self.i][self.j].type = 9  # destruction variable selon batiment_autres ressources specifiques (incomplet)      
 
 class Route(Batiment):
-    def __init__(self, i, j):
-        super().__init__(i, j)
+    def __init__(self):
+        super().__init__()
         self.type = 0
         self.materiaux_requis = 1
 
@@ -108,25 +110,17 @@ class Route(Batiment):
         return(result)
 
     def creation(self):
-        if self.accept_construction() and self.check_ressource():
+        if self.accept_construction() And self.check_ressource():
             batiments[self.i][self.j].type = 0
 
     def destruction(self):
         batiments[self.i][self.j].type = 9  # destruction variable selon batiment_autres ressources specifiques (incomplet)
 
 
-maison1 = Maison(3,4)
 
 
-terrainvide = Vide()
-route0 = Route(2, 0)
-route1 = Route(2, 1)
-route2 = Route(2, 2)
 
-batiments=[[terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide],
-           [terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide],
-           [route0,route1,route2,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide],
-           [terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide,terrainvide]]
+
 
      
                  
