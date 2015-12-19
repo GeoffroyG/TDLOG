abea2c9895068521cb46011271e9e35168b43af4
GeoffroyG <geoffroy.gallier@wanadoo.fr>
2015-12-19 17:57:45 +0100

    Déplacement sur la carte ajouté

--- Constantes.py
+++ Constantes.py
@@ -7,18 +7,26 @@ Created on Wed Dec 16 15:36:10 2015
 
 import pygame
 
-FPS = 30 # frames per second, the general speed of the program
-BOXSIZE = 40 # size of box height & width in pixels
-GAPSIZE = 5 # size of gap between boxes in pixels
-BOARDWIDTH = 10 # number of columns of icons
-BOARDHEIGHT = 10 # number of rows of icons
-GAMEWIDTH = BOARDWIDTH * (GAPSIZE + BOXSIZE) + GAPSIZE # size of game's width in pixels
-GAMEHEIGHT = BOARDHEIGHT * (GAPSIZE + BOXSIZE) + GAPSIZE # size of game' height in pixels
+FPS           = 30 # frames per second, the general speed of the program
+
+BOXSIZE       = 40 # size of box height & width in pixels
+GAPSIZE       = 5 # size of gap between boxes in pixels
+
+NBCOLUMN      = 30 # number of columns of icons in the map
+NBROW         = 30 # number of rows of icons in the map
+NBCOLUMN_DISP = 15 # number of columns displayed in the game windows
+NBROW_DISP    = 15 # number of rows displayed in the game windows
+
+GAMEWIDTH  = NBCOLUMN_DISP * (GAPSIZE + BOXSIZE) + GAPSIZE # size of game's width in pixels
+GAMEHEIGHT = NBROW_DISP    * (GAPSIZE + BOXSIZE) + GAPSIZE # size of game's height in pixels
+
 RESSOURCEBARWIDTH = GAMEWIDTH # size of the bar indicating the ressources
 RESSOURCEBARHEIGHT = 60 # size of the bar indicating the ressources
+
 MENUBARWIDTH = 180 # size of the bar indicating the menu
 MENUBARHEIGHT = RESSOURCEBARHEIGHT + GAPSIZE + GAMEHEIGHT # size of the bar indicating the menu
-WINDOWWIDTH = MENUBARWIDTH + 3*GAPSIZE + GAMEWIDTH  # final size of the window in pixels
+
+WINDOWWIDTH  = MENUBARWIDTH + 3*GAPSIZE + GAMEWIDTH  # final size of the window in pixels
 WINDOWHEIGHT = MENUBARHEIGHT + 2*GAPSIZE # final size of the window in pixels
 
 Graphism = ["2.Images/Road.png", "2.Images/House.png", "2.Images/Factory.png", "2.Images/None.png", "2.Images/None.png", "2.Images/None.png", "2.Images/None.png", "2.Images/None.png", "2.Images/None.png", "2.Images/Grass.png"]
--- Graphismes_test.py
+++ Graphismes_test.py
@@ -2,19 +2,20 @@
 """
 Created on Wed Dec 16 15:36:10 2015
 
-@author: Fatma - Geoffroy - Pierre 
+@author: Fatma - Geoffroy - Pierre
 """
 
 import Classes_Tests
 from Constantes import *
 from pygame.locals import *
+import os
 
 n = 3 # number of pictures per column in the menu
 
 Buildings = [Classes_Tests.Road(), Classes_Tests.House(), Classes_Tests.Factory(), Classes_Tests.Empty(), Classes_Tests.Empty(), Classes_Tests.Empty(), Classes_Tests.Empty(), Classes_Tests.Empty(), Classes_Tests.Empty(), Classes_Tests.Empty()]
 # Graphism and Buildings are to be modified together, one is the buidings list the other the pictures list
 
-mainBoard = Classes_Tests.Map(BOARDHEIGHT,BOARDWIDTH)
+mainBoard = Classes_Tests.Map(NBROW,NBCOLUMN)
 
 def main():
     global FPSCLOCK, DISPLAYSURF, Selected, building
@@ -26,13 +27,15 @@ def main():
     mousex = 0 # used to store x coordinate of mouse event
     mousey = 0 # used to store y coordinate of mouse event
     pygame.display.set_caption('Jeu Test') # name of the game
-    
+
+    origin = [0,0] # coordinates of the top-left cell displayed
+
     BGCOLOR = (255, 255, 255) # white background
     DISPLAYSURF.fill(BGCOLOR)
     game = False
-    Rules = False
+    rules = False
     posReturn = pygame.Rect(0,0,0,0)
-    
+
     while not game:
         background = pygame.image.load("2.Images/Backgroundimage.jpg").convert()
         pygame.transform.scale(background, (WINDOWWIDTH, WINDOWHEIGHT))
@@ -40,7 +43,7 @@ def main():
         text = font_title.render("Simulation de ville 2D", 1, (255, 0, 0))
         textposTitle = text.get_rect(centerx = WINDOWWIDTH / 2, centery = 50)
         DISPLAYSURF.blit(text, textposTitle)
-        if not Rules:
+        if not rules:
             text = font_other.render("Nouveau Jeu", 1, (10,10,10), (255,255,255))
             textposNewGame = text.get_rect(centerx = WINDOWWIDTH / 2, centery = WINDOWHEIGHT / 2)
             DISPLAYSURF.blit(text, textposNewGame)
@@ -54,24 +57,24 @@ def main():
             back = pygame.image.load("2.Images/Return.png").convert()
             DISPLAYSURF.blit(back, (0,0))
             posReturn = pygame.Rect(0, 0, 40, 40)
-            
-            
-        for event in pygame.event.get(): # event handling loop        
+
+
+        for event in pygame.event.get(): # event handling loop
             if event.type == MOUSEBUTTONUP:
                 mousex, mousey = event.pos
                 mouseClicked = True
             elif event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 pygame.quit()
-                sys.exit() 
-        
-        
-        if textposNewGame.collidepoint(mousex, mousey) and not Rules:
+                os.sys.exit()
+
+
+        if textposNewGame.collidepoint(mousex, mousey) and not rules:
             game = True
-        elif textposRules.collidepoint(mousex, mousey) and not Rules:
-            Rules = True
-        elif posReturn.collidepoint(mousex, mousey) and Rules:
-            Rules = False
-            
+        elif textposRules.collidepoint(mousex, mousey) and not rules:
+            rules = True
+        elif posReturn.collidepoint(mousex, mousey) and rules:
+            rules = False
+
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
@@ -79,15 +82,15 @@ def main():
 
     Selected = [False, False, False, False, False, False, False, False, False, False]
     building = Classes_Tests.Empty()
-    
+
     buildingSelected = False
     mouseClicked = False
-    # initialisation of the selection variables   
-    
+    # initialisation of the selection variables
+
     timer = 0 # set of a timer in frames
     timing = 0 # set of a timer in seconds to manipulate production
     Factories = [] # preparation of a list to store the factories for production
-    Factories_coords = []    
+    Factories_coords = []
     Houses = [] # same for the houses
     Houses_coords = [] # useful to keep coordinates as hab max depend on the proximity of Factories
     Roads = [Classes_Tests.Road()]
@@ -98,10 +101,10 @@ def main():
     while True: # main game loop
 
         DISPLAYSURF.fill(BGCOLOR) # drawing the window
-        drawBoard(mainBoard, DISPLAYSURF, Selected, timing)
-                
-                
-        for event in pygame.event.get(): # event handling loop        
+        drawBoard(mainBoard, DISPLAYSURF, Selected, timing, origin)
+
+
+        for event in pygame.event.get(): # event handling loop
             if event.type == MOUSEBUTTONUP:
                 mousex, mousey = event.pos
                 mouseClicked = True
@@ -109,11 +112,25 @@ def main():
                 mousex, mousey = event.pos
             elif event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 pygame.quit()
-                sys.exit()        
-        
-        
+                sys.exit()
+            
+            # Shifts on the map
+            if event.type == KEYUP and event.key == K_LEFT:
+                if origin[1] > 0:
+                    origin[1] -= 1
+            if event.type == KEYUP and event.key == K_RIGHT:
+                if origin[1] < NBCOLUMN - NBCOLUMN_DISP:
+                    origin[1] += 1
+            if event.type == KEYUP and event.key == K_UP:
+                if origin[0] > 0:
+                    origin[0] -= 1
+            if event.type == KEYUP and event.key == K_DOWN:
+                if origin[0] < NBROW - NBROW_DISP:
+                    origin[0] += 1
+
+
         if isInGame(mousex, mousey):
-            boxx, boxy = getBoxAtPixelGame(mousex, mousey)
+            boxx, boxy = getBoxAtPixelGame(mousex, mousey, origin)
             # If we're in the game, the coordinates represent a box and we have selected a building
             if boxx != None and boxy != None and mouseClicked and buildingSelected:
                 build = mainBoard.insert(building, boxx, boxy)
@@ -138,7 +155,7 @@ def main():
                     if building.type == 9:
                         remove([Roads, Roads_coords, Houses, Houses_coords, Factories, Factories_coords], boxx, boxy)
                         mainBoard.delete(boxx, boxy)
-                    
+
                 buildingSelected = False
                 building = Classes_Tests.Empty()
                 Selected = [False, False, False, False, False, False, False, False, False, False]
@@ -159,13 +176,13 @@ def main():
                 #text = font.render("employes : "+str(Factories[index].worker)+" \n "+"production : "+str(int(Factories[index].prod_max * Factories[index].worker / Factories[index].hab_max)), 1, (10,10,10))
                 #textpos = text.get_rect()
                 #DISPLAYSURF.blit(text, textpos)
-    
+
         if isInMenu(mousex):
             # Step to select a building in the menu
             if mouseClicked and getBuildingFromMenu(mousex, mousey, Selected) != None:
                 buildingSelected = True
                 building = getBuildingFromMenu(mousex, mousey, Selected)
-        
+
         # Reinitialization of the parameters after
         boxx, boxy = None, None
         mouseClicked = False
@@ -190,18 +207,18 @@ def main():
                 Factories[i].worker = min(worker_aux,Factories[i].hab_max)
                 mainBoard.wood += Factories[i].production(timer)
                 worker_aux -= Factories[i].worker
-                i+=1 # increment to go through all factories  
+                i+=1 # increment to go through all factories
         # Increase of the timer
         timer += 1
         timing = timer // FPS
 
 
-def getBoxAtPixelGame(mousex, mousey):
+def getBoxAtPixelGame(mousex, mousey, origin):
     # Translates the coordinates in the game from pixels to integers
-    j = mousex / GAMEWIDTH * BOARDWIDTH
-    i = (mousey - RESSOURCEBARHEIGHT) / GAMEHEIGHT * BOARDHEIGHT
+    j = (mousex / GAMEWIDTH * NBCOLUMN_DISP) + origin[1]
+    i = ((mousey - RESSOURCEBARHEIGHT) / GAMEHEIGHT * NBROW_DISP) + origin[0]
     return(int(i),int(j))
-    
+
 def getBuildingFromMenu(mousex, mousey, Selected):
     # Gives the building from the coordinates in the menu (see drawBoard)
     p = (mousex-WINDOWWIDTH+MENUBARWIDTH)//(GAPSIZE+BOXSIZE)
@@ -214,11 +231,11 @@ def getBuildingFromMenu(mousex, mousey, Selected):
         return(Buildings[k])
     else:
         return(None)
-        
+
 def getType(Map, boxx, boxy):
     Matrix = Map.types()
     return(Matrix[boxx][boxy])
-    
+
 def remove(list_types_coords, boxx, boxy):
     # The list is organized this way : [Building A, Building Coords A, Building B...]
     n = len(list_types_coords)
@@ -234,16 +251,16 @@ def remove(list_types_coords, boxx, boxy):
                 i_final = i
     del list_types_coords[2*k_final][i_final]
     del list_types_coords[2*k_final+1][i_final]
-        
 
-def drawBoard(Map, DISPLAYSURF, Selected, timing):
+
+def drawBoard(Map, DISPLAYSURF, Selected, timing, origin):
     # First we translate the map matrix into a types matrix
     Matrix = Map.types()
     # Then we display each box with the picture attached to it
     # Check how we can manage to win time by only displaying the changes
-    for i in range(BOARDHEIGHT):
-        for j in range(BOARDWIDTH):
-            p = Matrix[i][j]
+    for i in range(NBROW_DISP):
+        for j in range(NBCOLUMN_DISP):
+            p = Matrix[origin[0]+i][origin[1]+j]
             file = Graphism[p]
             dessin = pygame.image.load(file).convert()
             DISPLAYSURF.blit(dessin, (GAPSIZE + (BOXSIZE+GAPSIZE)*j, RESSOURCEBARHEIGHT + GAPSIZE + (BOXSIZE+GAPSIZE)*i))
@@ -265,7 +282,7 @@ def drawBoard(Map, DISPLAYSURF, Selected, timing):
     i=0
     for k in Graphism:
         dessin = pygame.image.load(k).convert()
-        # The formula is more understandable in this order, but it's the same than in detBuildingFromMenu actually 
+        # The formula is more understandable in this order, but it's the same than in detBuildingFromMenu actually
         x = (i//n)*(GAPSIZE+BOXSIZE)+WINDOWWIDTH-MENUBARWIDTH
         y = (i%n)*(GAPSIZE+BOXSIZE)+RESSOURCEBARHEIGHT+2*GAPSIZE
         DISPLAYSURF.blit(dessin,(x,y))
@@ -273,13 +290,13 @@ def drawBoard(Map, DISPLAYSURF, Selected, timing):
     i=0
     for k in Selected:
         if k:
-            graph = Graphism_Selected[i]           
+            graph = Graphism_Selected[i]
             dessin = pygame.image.load(graph).convert()
             x = (i//n)*(GAPSIZE+BOXSIZE)+WINDOWWIDTH-MENUBARWIDTH
             y = (i%n)*(GAPSIZE+BOXSIZE)+RESSOURCEBARHEIGHT+2*GAPSIZE
             DISPLAYSURF.blit(dessin,(x,y))
-        i+=1            
-    
+        i+=1
+
 
 def isInMenu(mousex):
     # returns True if the mouse is in the menu
@@ -294,4 +311,4 @@ def isInGame(mousex, mousey):
         return(True)
     else:
         return(False)
-    
+
