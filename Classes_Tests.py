97ff73dcbcf932d40eac10d55845dcf2b4092edd
FatmaHADDAD90 <haddad.fatma90@gmail.com>
2016-01-06 23:31:54 +0100

    Update Classes_Tests.py

--- Classes_Tests.py
+++ Classes_Tests.py
@@ -10,14 +10,18 @@ Created on Wed Dec 16 15:36:10 2015
 0 designe une route sur l'interface "batiments"
 1 designe une maison sur l'interface "batiments"
 2 designe une usine sur l'interface "batiments"
-3 designe une mine sur l'interface "batiments"
-4 designe un atelier sur l'interface "batiments"
+10 designe une mine sur l'interface "batiments"
+3 designe un atelier sur l'interface "batiments"
+4 designe une eolienne sur l'interface "batiments"
+5 designe une centrale Ã  charbon sur l'interface "batiments"
+6 designe une centrale nucleaire sur l'interface "batiments"
+7 designe une centrale hydraulique sur l'interface "batiments"
 """
 
 from Constantes import *
 import random
 
-# wood = 100 # evolution avec le temps => passÃ© en argument de la map
+# wood = 100 # evolution avec le temps => passÃƒÂ© en argument de la map
 workers_remaining = 0 # evolution avec le temps
 
 
@@ -27,21 +31,26 @@ class Building():
         self.broken_rate = 0
         self.isBroken = False
         self.wood_needed = 0
+        self.stone_needed = 0
 
 class Empty(Building):
     def __init__(self):
         self.type = 9
         self.wood_needed = 0
-
+        self.stone_needed = 0
+        
 class Road(Building):
     def __init__(self):
         self.type = 0
-        self.wood_needed = 1
+        self.wood_needed = 0
+        self.stone_needed = 1        
         self.time = 0
-
-    def check_ressource(self, wood):
+        self.cost= 100
+        self.elec_consumption = 0
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
         result = False
-        if wood >= self.wood_needed:
+        if stone >= self.stone_needed and money>= self.cost:
             result = True
         return(result)
         
@@ -52,12 +61,17 @@ class House(Building):
         self.hab_cond = 5
         self.hab = 0
         self.wood_needed = 5
+        self.stone_needed = 5
         self.time = 0
         self.debit = 40
-
-    def check_ressource(self, wood):
+        self.cost= 200
+        self.elec_consumption = 5 # in MW
+        self.real_consumption = 0  
+            
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
         result = False
-        if wood >= self.wood_needed:
+        if wood >= self.wood_needed and stone >= self.stone_needed and money >= self.cost and available_electricity >= self.elec_consumption :
             result = True
         return(result)
         
@@ -70,22 +84,25 @@ class House(Building):
 class Factory(Building):
     def __init__(self):
         self.type = 2
-        self.wood_needed = 10
-        
+        self.wood_needed = 0
+        self.stone_needed = 10
+        self.cost= 300
         self.hab_max = 10
-
+        self.VA = 100 # money +++
         self.prod_max = 10
         self.worker = 0
         self.debit = 80
         self.time = 0
-
-    def check_ressource(self, wood):
+        self.elec_consumption = 10 # in MW
+        self.real_consumption = 0
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
         result = False
-        if wood >= self.wood_needed:
+        if stone >= self.stone_needed and money>= self.cost and available_electricity >= self.elec_consumption:
             result = True
         return(result)
     
-    def production(self, timing): # La petite fonction de prod tranquilou! Simpliste, mais pour le cas dÃ©gÃ©nÃ©rÃ© on est bons
+    def production(self, timing): # La petite fonction de prod tranquilou! Simpliste, mais pour le cas dÃƒÂ©gÃƒÂ©nÃƒÂ©rÃƒÂ© on est bons
         wood = 0
         if (timing-self.time)%self.debit == 0:
             wood = int(self.prod_max * self.worker / self.hab_max)
@@ -96,24 +113,93 @@ class Mine(Building):
         self.type = 10
         self.stock = 1000
         self.wood_needed = 0
-
+        self.stone_needed = 0
+        
 class Workshop(Building):
     def __init__(self):
         self.type = 3
-        self.wood_needed = 10        
-
+        self.wood_needed = 10 
+        self.stone_needed = 10       
         self.hab_max = 10
-
+        self.cost= 200
         self.prod_max = 10
         self.worker = 0
         self.debit = 80
         self.time = 0
-    
-    def check_ressource(self, wood):
+        self.elec_consumption = 5
+        self.real_consumption = 0
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
         result = False
-        if wood >= self.wood_needed:
+        if wood >= self.wood_needed and stone >= self.stone_needed and money>= self.cost and available_electricity >= self.elec_consumption:
             result = True
         return(result)
+
+class Wind_power_plant(Building):
+    def __init__(self):
+        self.type = 4
+        self.capacity = 20 # 20MW in 10 mph winds, assuming wind constant
+        self.cost= 8000
+        self.wood_needed = 10
+        self.stone_needed = 10
+        self.time = 0
+        self.elec_consumption = 0
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
+        result = False
+        if wood >= self.wood_needed and stone >= self.stone_needed and money>= self.cost:
+            result = True
+        return(result)
+        
+class Coal_power_plant(Building):
+    def __init__(self):
+        self.type = 5
+        self.capacity = 70 # 70 MW
+        self.cost= 17000 
+        self.wood_needed = 15
+        self.stone_needed = 15        
+        self.time = 0
+        self.elec_consumption = 0
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
+        result = False
+        if wood >= self.wood_needed and stone >= self.stone_needed and  money>= self.cost:
+            result = True
+        return(result) 
+        
+class Nuclear_power_plant(Building):
+    def __init__(self):
+        self.type = 6
+        self.capacity = 300 # 300 MW
+        self.cost= 145000
+        self.wood_needed = 15
+        self.stone_needed = 15          
+        self.time = 0
+        self.elec_consumption = 0
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
+        result = False
+        if wood >= self.wood_needed and stone >= self.stone_needed and money>= self.cost:
+            result = True
+        return(result)
+        
+class  Hydraulic_power_plant(Building):
+    def __init__(self):
+        self.type = 7
+        self.capacity = 150 # 150 MW
+        self.cost= 27500
+        self.wood_needed = 15
+        self.stone_needed = 15          
+        self.time = 0
+        self.elec_consumption = 0
+        
+    def check_ressource(self, wood, stone, money, available_electricity):
+        result = False
+        if wood >= self.wood_needed and stone >= self.stone_needed and money>= self.cost:
+            result = True
+        return(result)    
+
+
 
 class Map():
     def __init__(self, height, width):
@@ -130,11 +216,19 @@ class Map():
         self.map[2][0] = Road()
         # J'ai rajoute quelques trucs qui seront plus manipulables en tant qu'arguments    
         self.wood = 100
+        self.stone = 100        
         self.habitants = 0
         self.height = height
         self.width = width
         self.workers = 0
-
+        self.money = 30000 # 30000$
+        self.electricity = 0
+        self.available_electricity = 0
+        self.capacity_electricity = 0
+        self.global_demand_elec = 0
+        self.remaining_demand_elec = 0
+        self.served_demand_elec = 0
+        
     def set_mines(self):
         ''' Creates random mines. '''
         for i in range(NBMINES):
@@ -184,9 +278,12 @@ class Map():
 
     def insert(self, building, i, j):
         ''' Inserts a building in cell [i][j]. '''
-        if self.check_empty(i, j) and self.check_road_junction(i, j) and building.check_ressource(self.wood):            
+        if self.check_empty(i, j) and self.check_road_junction(i, j) and (building.check_ressource(self.wood,self.stone,self.money,self.available_electricity)):            
             self.map[i][j] = building
             self.wood -= building.wood_needed
+            self.stone -= building.stone_needed
+            self.money -= building.cost
+            self.available_electricity -= building.elec_consumption
             return(True)
         return(False)
         
